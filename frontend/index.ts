717dd6dfa686c052366e93aae452b796bd350133
YAMIPC <97910311+SRV013@users.noreply.github.com>
2023-01-06 09:05:21 -0300

    Update index.ts

--- frontend/index.ts
+++ frontend/index.ts
@@ -1,20 +1,13 @@
 import "./router";
 import "./componentes/header";
 import "./componentes/ubicacion";
-
 import "./pages/login";
 import "./pages/inicio";
-
 import "./pages/usuario_nuevo";
 import "./pages/usuario_actualiza";
-
 import "./pages/mascotas";
 import "./pages/mascota_nueva";
 import "./pages/mascota_editar";
-
 import "./pages/perdidas";
 import "./pages/reporte_vista";
-
 import "./pages/mensaje";
-
-console.log("index.ts");
