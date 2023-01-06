a59abaced87ba7f6c55e11fd39dbb04bbcfba00e
YAMIPC <97910311+SRV013@users.noreply.github.com>
2023-01-06 09:16:57 -0300

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -18,14 +18,11 @@ import { borrarTabla } from "./db/conexion";
 import * as cors from "cors";
 import * as express from "express";
 const app = express();
-const port = process.env.PORT || 8080;
-app.use(express.static("dist"));
-app.use(
-    express.json({
-        limit: "50mb",
-    })
-);
 app.use(cors());
+app.use(express.json({ limit: "75mb" }));
+app.use(express.json());
+const port = process.env.PORT || 8080;
+
 // NUEVO USUARIO
 app.post("/usuario", async (req, res) => {
     const { email, nombre, telefono, password } = req.body;
@@ -193,7 +190,13 @@ app.get("/borrar", async (req, res) => {
     const borrar = await borrarTabla();
     res.json(borrar);
 });
-app.get("*", express.static(__dirname + "../../be-dist"));
+
+const relativeRoute = path.resolve(__dirname, "../dist");
+app.use(express.static(relativeRoute));
+app.get("*", function(req, res){
+    res.sendFile(relativeRoute + "/index.html");
+})
+
 app.listen(port, () => {
     console.log("todo OK en el puerto:", port);
 });
